1c9118f46f65da6312b72d4b914e05267e711da7
enz <enz@2e953b5c-c64d-0410-be54-f773e93e544c>
2008-07-02 21:35:59 +0000

    Don't print warning, if multiple postfixes found, this is expected
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@222 2e953b5c-c64d-0410-be54-f773e93e544c

--- build/gmake/boost_postfix.sh
+++ build/gmake/boost_postfix.sh
@@ -7,26 +7,15 @@ ALL_LIBRARY_PATHS=/usr/local/lib:/usr/lib:$LIBRARY_PATH:$LD_LIBRARY_PATH
 SEARCH_PATHS=`echo $ALL_LIBRARY_PATHS | tr ':' '\n'`
 EXTENSIONS="-mt -gcc -gcc-mt"
 POSTFIX="-mt" # Default extension
-FOUND=False
 for LIB_PATH in $SEARCH_PATHS
 do
     for EXT in "" $EXTENSIONS
     do
         if [[ -e $LIB_PATH/libboost_thread$EXT.a || -e $LIB_PATH/libboost_thread$EXT.so ]]
         then
-            if [[ $FOUND == "True" ]]
-            then
-                echo "Warning: Multiple Boost postfixes found" >&2
-            fi
             POSTFIX=$EXT
-            FOUND=True
         fi
     done
 done
-
-if [[ $FOUND == "False" ]]
-then
-    echo "Warning: No Boost postfixes found" >&2
-fi
 
 echo $POSTFIX
