f617cd5ec123c49b89102ebf195602e36fc7ed1b
enz <enz@2e953b5c-c64d-0410-be54-f773e93e544c>
2008-07-09 17:49:57 +0000

    New attempt to trigger necessary recompilations if SVN version changes
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@235 2e953b5c-c64d-0410-be54-f773e93e544c

---
+++ build/gmake/update_version_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Creates or updates a file with the SVN version using the svnversion command.
+# The file date will be updated only, if the version has changed or the file
+# was newly created, such that the file can be used as a dependency in
+# makefiles. The content of the file is the SVN version without trailing
+# newline. The script writes the fully qualified path to the file to standard
+# output
+
+cd "$(dirname $0)"
+FILENAME="$PWD/build/version"
+SVNVERSION=$(svnversion -n ../..)
+if [ ! -e "$FILENAME" ] || [ $(cat "$FILENAME") != "$SVNVERSION" ]; then
+    echo -n "$SVNVERSION" > "$FILENAME"
+fi
+echo "$FILENAME"
