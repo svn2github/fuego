57a79e1ba626082edc4a985481f4ad707e558458
enz <enz@2e953b5c-c64d-0410-be54-f773e93e544c>
2008-07-17 15:48:12 +0000

    Use Benson solver; always generate capturing moves in own safe territory
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@275 2e953b5c-c64d-0410-be54-f773e93e544c

--- gouct/GoUctDefaultRootFilter.cpp
+++ gouct/GoUctDefaultRootFilter.cpp
@@ -7,10 +7,10 @@
 #include "SgSystem.h"
 #include "GoUctDefaultRootFilter.h"
 
+#include "GoBensonSolver.h"
 #include "GoBoard.h"
 #include "GoBoardUtil.h"
 #include "GoModBoard.h"
-#include "GoSafetySolver.h"
 
 using namespace std;
 
@@ -33,10 +33,11 @@ vector<SgPoint> GoUctDefaultRootFilter::Get()
 
     GoModBoard modBoard(m_bd);
     GoBoard& bd = modBoard.Board();
-    GoSafetySolver safetySolver(bd);
+    // Use Benson solver to ensure that all moves that capture dead stones
+    // are liberties of dead blocks
+    GoBensonSolver bensonSolver(bd);
     SgBWSet safe;
-    safetySolver.FindSafePoints(&safe);
-    bool captureDead = bd.Rules().CaptureDead();
+    bensonSolver.FindSafePoints(&safe);
     for (GoBoard::Iterator it(bd); it; ++it)
     {
         SgPoint p = *it;
@@ -45,10 +46,11 @@ vector<SgPoint> GoUctDefaultRootFilter::Get()
             bool isSafe = safe[toPlay].Contains(p);
             bool isSafeOpp = safe[opp].Contains(p);
             bool hasOppNeighbors = bd.HasNeighbors(p, opp);
-            // Filter moves in safe territory, but generate moves on liberties
-            // of opponent dead stones, if captureDead
-            // TODO: are other capturing moves possible?
-            if ((isSafe && (! captureDead || ! hasOppNeighbors)) || isSafeOpp)
+            // Always generate capturing moves in own safe territory, even
+            // if current rules do no use CaptureDead(), because the UCT
+            // player always scores with Tromp-Taylor after two passes in the
+            // in-tree phase
+            if ((isSafe && ! hasOppNeighbors) || isSafeOpp)
                 rootFilter.push_back(p);
         }
     }
