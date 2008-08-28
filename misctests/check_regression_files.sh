8d8db57d5d2112bf21f7322dd57eabbf62a7d24c
enz <enz@2e953b5c-c64d-0410-be54-f773e93e544c>
2008-08-28 21:18:31 +0000

    Add new directory for misc test scripts
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@492 2e953b5c-c64d-0410-be54-f773e93e544c

---
+++ misctests/check_regression_files.sh
@@ -0,0 +1,29 @@
+# Check that all SGF files references by a test in regression/ exist and that
+# regression/Makefile.am contains all tests and SGF files
+
+cd ../regression
+
+echo -n "Checking that regression/Makefile.am contains all tests... "
+for TSTFILE in `find . -name "*.tst" -o -name "*.list" | sed s,^./,,`; do
+   if ! grep -q "$TSTFILE" Makefile.am; then
+       echo "fail"
+       echo "regression/Makefile.am does not contain $TSTFILE"
+       exit 1;
+   fi
+done
+echo "ok"
+
+echo -n "Checking that regression/Makefile.am contains all SGF files... "
+for SGFFILE in `grep loadsgf *.tst|awk '{print $2}'`; do
+   if ! test -f $SGFFILE; then
+       echo "fail"
+       echo "Non-existing file $SGFFILE is referenced by a regression test"
+       exit 1;
+   fi
+   if ! grep -q "$SGFFILE" Makefile.am; then
+       echo "fail"
+       echo "regression/Makefile.am does not contain $SGFFILE"
+       exit 1;
+   fi
+done
+echo "ok"
