c808ce8f1a77748caa79282449f51cc443b833d7
enz <enz@2e953b5c-c64d-0410-be54-f773e93e544c>
2008-10-06 21:01:35 +0000

    Add documentation
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@576 2e953b5c-c64d-0410-be54-f773e93e544c

--- doc/general/pages/autotools.cpp
+++ doc/general/pages/autotools.cpp
@@ -1,11 +1,15 @@
 /** @page generalautotools Building Fuego using GNU Autotools
 
-@section generalautotoolsdistro Building a distribution (released version)
+@section generalautotoolsdistro Building from a distribution (released version)
 
 @verbatim
 ./configure
 make
 @endverbatim
+
+Interesting options to configure are @c --enable-assert for enabling
+assertions or @c --enable-optimize=no for switching off optimizations.
+<tt>./configure --help</tt> returns a full list of options.
 
 @section generalautotoolssvn Building a development version checked out from SVN
 
@@ -28,15 +32,41 @@ A better way is to configure your makefiles with
 check, if <tt>configure.in</tt> or a <tt>Makefile.am</tt> have changed and
 recreate the makefiles bevor the compilation if necessary.
 
-Other interesting options to configure are @c --enable-assert for enabling
-assertions or @c --enable-optimize=no for switching off optimizations.
-<tt>./configure --help</tt> returns a full list of options.
-
 @section generalautotoolsinstall Installing Fuego
 
 @verbatim
 sudo make install
 @endverbatim
 
+@section generalautotoolvpath Building debug and release version
+
+Assuming that you want to build a debug and release version in different
+directories. This is called a VPATH-build. In this example, we choose
+<tt>fuego/build/autotools/debug</tt> and
+<tt>fuego/build/autotools/release</tt> as the build directories:
+@verbatim
+cd fuego
+mkdir -p build/autotools/debug
+cd build/autotools/debug
+../../../configure --enable-assert --enable-optimize=no
+
+cd ../../../
+mkdir -p build/autotools/release
+../../../configure
+@endverbatim
+Then the command <tt>make</tt> should be run in
+<tt>fuego/build/autotools/debug</tt> or
+<tt>fuego/build/autotools/release</tt>.
+
+@section generalautotoollinks Further documentation links
+
+- <a href="http://www.freesoftwaremagazine.com/books/autotools_a_guide_to_autoconf_automake_libtool">Autotools: a practitioner's guide to Autoconf, Automake and Libtool</a>
+- <a href="http://sources.redhat.com/autobook/autobook/autobook_toc.html">GNU Autoconf, Automake, and Libtool</a>
+  (some of the examples are not yet updated to newer versions of Autotools;
+  still a useful reference)
+- GNU Autotools manuals:
+  <a href="http://www.gnu.org/software/automake/manual/html_node/index.html">Automake</a>,
+  <a href="http://www.gnu.org/software/autoconf/manual/html_node/index.html">Autoconf</a>,
+  <a href="http://www.gnu.org/software/libtool/manual/html_node/index.html">Libtool</a>
 */
 
