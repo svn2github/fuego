ba062d8dd97827ac85f61db63bb0f28c1372b8a6
enz <enz@2e953b5c-c64d-0410-be54-f773e93e544c>
2008-11-06 22:41:56 +0000

    Use option -nobook
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@627 2e953b5c-c64d-0410-be54-f773e93e544c

--- regression/run.sh
+++ regression/run.sh
@@ -15,7 +15,7 @@ if [[ "$FUEGO_BUILD_ROOT" == "" ]]; then
         FUEGO_BUILD_ROOT="../build/autotools/debug"
     fi
 fi
-FUEGO="$FUEGO_BUILD_ROOT/fuegomain/fuego -srand 1"
+FUEGO="$FUEGO_BUILD_ROOT/fuegomain/fuego -nobook -srand 1"
 FUEGOTEST="$FUEGO_BUILD_ROOT/fuegotest/fuego_test -srand 1"
 
 #-----------------------------------------------------------------------------
