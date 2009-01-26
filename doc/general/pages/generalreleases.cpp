2de427865d4de524a7b7f309f79b2669d53a78d3
enz <enz@2e953b5c-c64d-0410-be54-f773e93e544c>
2009-01-26 23:53:12 +0000

    Extend
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@765 2e953b5c-c64d-0410-be54-f773e93e544c

--- doc/general/pages/generalreleases.cpp
+++ doc/general/pages/generalreleases.cpp
@@ -8,21 +8,38 @@ This is a short HOWTO for maintainers how to do a Fuego release.
    configure.ac and update the current section header in NEWS
 -# Commit the changes
 -# Tag the current version.
-Example: for release 0.3.1 in the bugfix branch of version 0.3, the command
-would be:
+   Example 1: for release 0.3.1 in the bugfix branch of version 0.3, the
+   command would be:
 @verbatim
 svn copy  \
 https://fuego.svn.sourceforge.net/svnroot/fuego/branches/VERSION_0_3_FIXES  \
 https://fuego.svn.sourceforge.net/svnroot/fuego/tags/VERSION_0_3_1 \
 -m "Tag release 0.3.1"
+@verbatim
+@endverbatim
+   Example 2: for release 0.3 (a new major release), the command would be:
+@verbatim
+svn copy  \
+https://fuego.svn.sourceforge.net/svnroot/fuego/trunk  \
+https://fuego.svn.sourceforge.net/svnroot/fuego/tags/VERSION_0_3 \
+-m "Tag release 0.3.1"
 @endverbatim
 -# Run <tt>autoreconf -i</tt> in the root directory of Fuego and then
    <tt>make dist</tt>. The file release is now in the current directory
 -# If the release is a new major release, create a bugfix branch.
--# Change the version in configure.ac to a developemnt version and add a new
+   Example: for release 0.3, the command would be:
+@verbatim
+svn copy  \
+https://fuego.svn.sourceforge.net/svnroot/fuego/trunk  \
+https://fuego.svn.sourceforge.net/svnroot/fuego/branches/VERSION_0_3_FIXES \
+-m "Create bugfix branch for release 0.3"
+@endverbatim
+-# Change the version in configure.ac to a development version and add a new
    section header in NEWS ("current development version").
    The development version string is by convention the version of the last
    release with ".SVN" appended.
+   After a new major release, you need to change the version string in both the
+   trunk and the new bugfix branch.
    To distinguish between trunk and bugfix branch, the first development
    version in the bugfix branch has a ".0.SVN" appended.
 -# Commit the changes.
