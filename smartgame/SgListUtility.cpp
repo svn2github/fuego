1056cf1a985bafb0fd6b3a8732e4b1938b17b9ba
mmueller65 <mmueller65@2e953b5c-c64d-0410-be54-f773e93e544c>
2009-04-25 22:12:11 +0000

    continue implementation of SgVector
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@839 2e953b5c-c64d-0410-be54-f773e93e544c

--- smartgame/SgListUtility.cpp
+++ smartgame/SgListUtility.cpp
@@ -18,7 +18,7 @@ void SgListUtility::Intersection(SgList<int>* list, const SgList<int>& list2)
      SgList<int> newList;
      for (SgListIterator<int> it(*list); it; ++it)
      {
-         // AR speed up by hash tags, if used in time-critical code
+         // @todo speed up by hash tags, if used in time-critical code
          if (list2.Contains(*it))
              newList.Append(*it);
      }
@@ -51,4 +51,16 @@ void SgVectorUtility::Difference(SgVector<int>* list, const SgVector<int>& list2
     // @todo speed up by using tags, if used in time-critical code.
     for (SgVectorIterator<int> it(list2); it; ++it)
         list->Exclude(*it);
+}
+
+void SgVectorUtility::Intersection(SgVector<int>* list, const SgVector<int>& list2)
+{
+     SgVector<int> newList;
+     for (SgVectorIterator<int> it(*list); it; ++it)
+     {
+         // @todo speed up by hash tags, if used in time-critical code
+         if (list2.Contains(*it))
+             newList.Append(*it);
+     }
+     newList.SwapWith(list);
 }
