023cc3d15730b29f57a77ac974da8599dce0190b
mmueller65 <mmueller65@2e953b5c-c64d-0410-be54-f773e93e544c>
2011-01-12 21:04:07 +0000

    return 0 for TotalMemory() if _SC_PHYS_PAGES not defined
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@1343 2e953b5c-c64d-0410-be54-f773e93e544c

--- smartgame/SgPlatform.cpp
+++ smartgame/SgPlatform.cpp
@@ -22,6 +22,7 @@ std::size_t SgPlatform::TotalMemory()
         return 0;
     return static_cast<size_t>(status.ullTotalPhys);
 #else
+#ifdef _SC_PHYS_PAGES
     long pages = sysconf(_SC_PHYS_PAGES);
     if (pages < 0)
         return 0;
@@ -29,6 +30,9 @@ std::size_t SgPlatform::TotalMemory()
     if (pageSize < 0)
         return 0;
     return static_cast<size_t>(pages) * static_cast<size_t>(pageSize);
+#else
+	return 0;
+#endif
 #endif
 }
 
