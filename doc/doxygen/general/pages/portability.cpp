8075ac2ff3373b6f0e00c086898af5aeb4ef4afe
enz <enz@2e953b5c-c64d-0410-be54-f773e93e544c>
2011-01-13 13:26:07 +0000

    Updated documentation
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@1353 2e953b5c-c64d-0410-be54-f773e93e544c

--- doc/doxygen/general/pages/portability.cpp
+++ doc/doxygen/general/pages/portability.cpp
@@ -9,6 +9,6 @@ of platforms. Currently, Fuego uses only features existing in Boost version
 
 The implementation of functionality that is not available in the standard
 C++ libraries or Boost is encapsulated in classes in the SmartGame library
-(e.g. SgTime, SgProcess). The current implementation using POSIX functions.
-To port these classes to other platforms, an alternative implementation needs
-to be added to the source code. */
+(e.g. SgTime, SgProcess). The current implementation supports Windows and
+systems supporting the POSIX standard. To port these classes to other
+platforms, alternative implementations need to be added to the source code. */
