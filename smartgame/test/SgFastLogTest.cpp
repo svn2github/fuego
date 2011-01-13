d70837b670ed2708386d063a419779a3ac7a74d9
enz <enz@2e953b5c-c64d-0410-be54-f773e93e544c>
2011-01-13 16:11:54 +0000

    Make SgFastLogTest compile on systems that do not define M_E in math.h
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@1355 2e953b5c-c64d-0410-be54-f773e93e544c

--- smartgame/test/SgFastLogTest.cpp
+++ smartgame/test/SgFastLogTest.cpp
@@ -10,6 +10,8 @@
 #include <boost/test/floating_point_comparison.hpp>
 #include "SgFastLog.h"
 
+using namespace std;
+
 //----------------------------------------------------------------------------
 
 namespace {
@@ -18,7 +20,7 @@ BOOST_AUTO_TEST_CASE(SgFastLogTest_Basic)
 {
     SgFastLog fastLog(10);
     const float epsilon = 0.1f;
-    const float naturalLogBase = float(M_E);
+    const float naturalLogBase = exp(1.f);
     BOOST_CHECK_CLOSE(fastLog.Log(1), 0.0f, epsilon);
     BOOST_CHECK_CLOSE(fastLog.Log(naturalLogBase), 1.0f, epsilon);
     BOOST_CHECK_CLOSE(fastLog.Log(powf(naturalLogBase, 2.0f)), 2.0f, epsilon);
