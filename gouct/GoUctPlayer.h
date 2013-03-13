bdb621d3b40a456a3ce6e1276048558d1f379efe
mmueller65 <mmueller65@2e953b5c-c64d-0410-be54-f773e93e544c>
2013-03-13 05:25:19 +0000

    improve pass behavior under Japanese rules
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@1647 2e953b5c-c64d-0410-be54-f773e93e544c

--- gouct/GoUctPlayer.h
+++ gouct/GoUctPlayer.h
@@ -283,6 +283,8 @@ public:
     /** See ResignThreshold() */
     SgUctValue m_resignThreshold;
 
+    const SgUctValue m_sureWinThreshold;
+
     /** Used in OnBoardChange() */
     int m_lastBoardSize;
 
@@ -310,7 +312,8 @@ public:
 
     SgMove GenMovePlayoutPolicy(SgBlackWhite toPlay);
 
-    bool DoEarlyPassSearch(SgUctValue maxGames, double maxTime, SgPoint& move);
+    bool DoEarlyPassSearch(SgUctValue maxGames, double maxTime,
+                           SgPoint searchMove, SgPoint& move);
 
     SgPoint DoSearch(SgBlackWhite toPlay, double maxTime,
                      bool isDuringPondering);
@@ -551,6 +554,7 @@ GoUctPlayer<SEARCH, THREAD>::GoUctPlayer(const GoBoard& bd)
       m_useRootFilter(true),
       m_reuseSubtree(true),
       m_earlyPass(true),
+      m_sureWinThreshold(0.80f),
       m_lastBoardSize(-1),
       m_maxGames(std::numeric_limits<SgUctValue>::max()),
       m_resignMinGames(5000),
@@ -572,8 +576,7 @@ GoUctPlayer<SEARCH, THREAD>::GoUctPlayer(const GoBoard& bd)
 
 template <class SEARCH, class THREAD>
 GoUctPlayer<SEARCH, THREAD>::~GoUctPlayer()
-{
-}
+{ }
 
 template <class SEARCH, class THREAD>
 void GoUctPlayer<SEARCH, THREAD>::ClearStatistics()
@@ -581,15 +584,87 @@ void GoUctPlayer<SEARCH, THREAD>::ClearStatistics()
     m_statistics.Clear();
 }
 
+typedef SgPointArray<SgUctStatistics> TerrArray;
+
+namespace {
+bool HasStatsForAllMoves(const GoBoard& bd, const TerrArray& territory)
+{
+    for (GoBoard::Iterator it(bd); it; ++it)
+        if (territory[*it].Count() == 0)
+        {
+            // No statistics, maybe all simulations aborted due to
+            // max length or mercy rule.
+            SgDebug() << "GoUctPlayer: no early pass possible (no stat) for"
+                        << SgWritePoint(*it) << '\n';
+            return false;
+        }
+    return true;
+}
+
+// translate from Black's view to player's view
+inline SgUctValue ValueForPlayer(SgUctValue v, SgBlackWhite player)
+{
+    if (player == SG_WHITE)
+        return 1 - v; // use InverseEstimate? Everywhere?
+    else
+        return v;
+}
+    
+bool HasNonControlledLib(const GoBoard& bd, SgPoint block, SgBlackWhite toPlay,
+                     const TerrArray& territory, SgUctValue threshold)
+{
+    SG_ASSERT(bd.IsColor(block, toPlay));
+
+    const SgUctValue blockMean = ValueForPlayer(territory[block].Mean(),
+                                                toPlay);
+    if (blockMean < threshold) // block not safe, probably dead. No fillin.
+        // todo check for 1-threshold instead to check for dead blocks?
+        // what happens if unsettled blocks passed in here?
+        return false;
+
+    for (GoBoard::LibertyIterator it(bd, block); it; ++it)
+    {
+        const SgUctValue mean = ValueForPlayer(territory[*it].Mean(), toPlay);
+        if (mean < threshold)
+        {
+            SgDebug() << "non-controlled liberty " << SgWritePoint(*it)
+            << " of block " << SgWritePoint(block)
+            << " mean " << territory[*it].Mean()
+            << "\n";
+
+            return true;
+        }
+    }
+    return false;
+}
+
+bool AllowFillinMove(const GoBoard& bd, SgPoint move,
+                     const TerrArray& territory, SgUctValue threshold)
+{
+    /*  Idea: if adj. block has another liberty that is not controlled by us
+        - neutral or controlled by opponent (e.g. our selfatari)
+        then allow the move. Does not tactics-check it,
+        assumes this is the search move. */
+    SG_ASSERT(bd.IsEmpty(move));
+    const SgBlackWhite toPlay = bd.ToPlay();
+    for (GoNeighborBlockIterator it(bd, move, toPlay); it; ++it)
+        if (HasNonControlledLib(bd, *it, toPlay, territory, threshold))
+                return true;
+    return false;
+}
+
+} // namespace
+
 /** Perform a search after playing a pass and see if it is still a win and
     all points are safe as determined by territory statistics.
     @param maxGames Maximum simulations for the search
     @param maxTime Maximum time for the search
     @param[out] move The move to play (pass or a neutral point to fill)
-    @return @c true, if it is still a win and everything is safe after a pass */
+    @return @c true, if still winning and everything is safe after a pass */
 template <class SEARCH, class THREAD>
-bool GoUctPlayer<SEARCH, THREAD>::DoEarlyPassSearch(SgUctValue maxGames, 
+bool GoUctPlayer<SEARCH, THREAD>::DoEarlyPassSearch(SgUctValue maxGames,
                                                     double maxTime,
+                                                    SgPoint searchMove,
                                                     SgPoint& move)
 {
     SgDebug() << "GoUctPlayer: doing a search if early pass is possible\n";
@@ -615,7 +690,7 @@ bool GoUctPlayer<SEARCH, THREAD>::DoEarlyPassSearch(SgUctValue maxGames,
         std::vector<SgPoint> sequence;
         SgUctValue value = m_search.Search(maxGames, maxTime, sequence);
         value = m_search.InverseEstimate(value);
-        winAfterPass = (value > 1 - m_resignThreshold);
+        winAfterPass = (value > m_sureWinThreshold);
     }
     bd.Undo();
 
@@ -627,29 +702,29 @@ bool GoUctPlayer<SEARCH, THREAD>::DoEarlyPassSearch(SgUctValue maxGames,
     }
     move = SG_PASS;
     THREAD& threadState = dynamic_cast<THREAD&>(m_search.ThreadState(0));
-    SgPointArray<SgUctStatistics> territory =
-    threadState.m_territoryStatistics;
-    if (earlyPassPossible)
+    const TerrArray territory = threadState.m_territoryStatistics;
+    if (earlyPassPossible && ! HasStatsForAllMoves(bd, territory))
     {
-        for (GoBoard::Iterator it(bd); it; ++it)
-            if (territory[*it].Count() == 0)
-            {
-                // No statistics, maybe all simulations aborted due to
-                // max length or mercy rule.
-                SgDebug() 
-                    << "GoUctPlayer: no early pass possible (no stat)\n";
-                earlyPassPossible = false;
-                break;
-            }
+        earlyPassPossible = false;
     }
+    
+    if (earlyPassPossible
+        && AllowFillinMove(bd, searchMove, territory, m_sureWinThreshold))
+    {
+        move = searchMove;
+        SgDebug() << "GoUctPlayer: allow fill-in move "
+                    << SgWritePoint(move) << "\n";
+        return true;
+    }
+
 
     if (earlyPassPossible)
     {
-        const float threshold = 0.2f; // Safety threshold
         for (GoBoard::Iterator it(bd); it; ++it)
         {
             SgUctValue mean = territory[*it].Mean();
-            if (mean > threshold && mean < 1 - threshold)
+            if (  mean > 1 - m_sureWinThreshold
+               && mean < m_sureWinThreshold)
             {
                 // Check if neutral point
                 bool isSafeToPlayAdj = false;
@@ -657,9 +732,9 @@ bool GoUctPlayer<SEARCH, THREAD>::DoEarlyPassSearch(SgUctValue maxGames,
                 for (GoNbIterator it2(bd, *it); it2; ++it2)
                 {
                     SgUctValue mean = territory[*it2].Mean();
-                    if (mean < threshold)
+                    if (mean > m_sureWinThreshold)
                         isSafeToPlayAdj = true;
-                    if (mean > 1 - threshold)
+                    if (mean < 1 - m_sureWinThreshold)
                         isSafeOppAdj = true;
                 }
                 if (isSafeToPlayAdj && isSafeOppAdj)
@@ -749,7 +824,7 @@ SgPoint GoUctPlayer<SEARCH, THREAD>::DoSearch(SgBlackWhite toPlay,
     m_search.SetToPlay(toPlay);
     std::vector<SgPoint> sequence;
     SgUctEarlyAbortParam earlyAbort;
-    earlyAbort.m_threshold = 1.f - m_resignThreshold;
+    earlyAbort.m_threshold = m_sureWinThreshold;
     earlyAbort.m_minGames = m_resignMinGames;
     earlyAbort.m_reductionFactor = 3;
     SgUctValue value = m_search.Search(m_maxGames, maxTime, sequence, rootFilter,
@@ -791,14 +866,21 @@ SgPoint GoUctPlayer<SEARCH, THREAD>::DoSearch(SgBlackWhite toPlay,
         move = GoUctSearchUtil::TrompTaylorPassCheck(move, m_search);
     }
 
+    SgDebug() << "m_earlyPass " << m_earlyPass
+    << " wasEarlyAbort " << wasEarlyAbort
+    << " value " << value
+    << " m_resignThreshold " << m_resignThreshold
+    << " m_sureWinThreshold " << m_sureWinThreshold
+    << " earlyAbort.m_threshold " << earlyAbort.m_threshold
+    << '\n';
     // If SgUctSearch aborted early, use the remaining time/nodes for doing a
     // search, if an early pass is possible
-    if (m_earlyPass && wasEarlyAbort)
+    if (m_earlyPass && (wasEarlyAbort || value > m_sureWinThreshold))
     {
         maxTime -= timer.GetTime();
         SgPoint earlyPassMove;
         if (DoEarlyPassSearch(m_maxGames / earlyAbort.m_reductionFactor,
-                              maxTime, earlyPassMove))
+                              maxTime, move, earlyPassMove))
             move = earlyPassMove;
     }
 
@@ -1041,7 +1123,7 @@ bool GoUctPlayer<SEARCH, THREAD>::VerifyNeutralMove(SgUctValue maxGames,
     SgUctValue value = m_search.Search(maxGames, maxTime, sequence);
     value = m_search.InverseEstimate(value);
     bd.Undo();
-    return value >= 1 - m_resignThreshold;
+    return value >= m_sureWinThreshold;
 }
 
 //----------------------------------------------------------------------------
