21c736ee1284d9db538816630f4903f465980c52
mmueller65 <mmueller65@2e953b5c-c64d-0410-be54-f773e93e544c>
2013-03-16 07:56:40 +0000

    do not increment boolean
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@1655 2e953b5c-c64d-0410-be54-f773e93e544c

--- go/GoBoardUtil.cpp
+++ go/GoBoardUtil.cpp
@@ -52,9 +52,9 @@ void ScorePositionRecurse(const GoBoard& bd, SgPoint p,
     if (c != SG_EMPTY && ! deadStones.Contains(p))
     {
         if (c == SG_BLACK)
-            ++isBlackAdjacent;
+            isBlackAdjacent = true;
         else
-            ++isWhiteAdjacent;
+            isWhiteAdjacent = true;
         return;
     }
     if (! marker.NewMark(p))
