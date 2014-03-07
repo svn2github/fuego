cc4b9aa62c3cdec3b44e026cfbd0e3f5a111bcfa
mmueller65 <mmueller65@2e953b5c-c64d-0410-be54-f773e93e544c>
2014-03-07 00:19:46 +0000

    distributed training of features
    git-svn-id: svn://svn.code.sf.net/p/fuego/code/trunk@1890 2e953b5c-c64d-0410-be54-f773e93e544c

---
+++ tools/features/train/distribute.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+run() {
+mkdir -p $DIR
+cd $DIR
+../train.sh $YEARS&
+#../demo.sh $YEARS&
+cd ..
+}
+
+DIR=1
+YEARS="1[0-8]* 19[0-5]*"
+run
+DIR=2
+YEARS="19[6-7]* 198[0-3]"
+run
+DIR=3
+YEARS="198[4-9] 1990"
+run
+DIR=4
+YEARS="199[1-6]"
+run
+DIR=5
+YEARS="199[7-9] 2000"
+run
+DIR=6
+YEARS="200[1-3]"
+run
+DIR=7
+YEARS="200[4-8]"
+run
+DIR=8
+YEARS="2009 201[0-1]"
+run
+
+if [ 1 -eq 0 ]; then
+commented out
+10-1959
+1960-1983
+1984-1990
+1991-1996
+1997-2000
+2001-2003
+2004-2008
+2009-2011
+fi
---
+++ tools/features/train/train.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu
+
+if [[ $OSTYPE == darwin* ]]; then
+#mac
+COLLECTION=/Users/mmueller/svns/sgf/pro-19x19/badukmovies-pro-collection
+else
+#unix
+COLLECTION=/cshome/mmueller/projects/sgf/pro-19x19/badukmovies-pro-collection
+fi
+
+for YEARS in "$@"
+do
+echo training $YEARS in
+pwd
+for YEAR in `echo $COLLECTION/$YEARS/`; do
+echo $YEAR
+for GAMEDIR in `ls $YEAR/`; do
+echo $GAMEDIR
+gogui-statistics -program $FUEGO -commands "features_wistuba_file" -size 19 \
+-quiet -force $GAMEDIR/*sgf
+done
+done
+done
+
